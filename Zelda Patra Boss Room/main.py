341ca20254c594ea9ac38032e4331b8304758aae
NiteNova <ntam101@gmail.com>
2023-09-12 15:14:43 -0600

    a

--- Zelda Patra Boss Room/main.py
+++ Zelda Patra Boss Room/main.py
@@ -1,1 +1,13 @@
 import pygame
+import random
+
+pygame.init()
+pygame.display.set_caption("Boss Room")
+screen = pygame.display.set_mode((1000, 1000))
+clock = pygame.time.Clock()
+
+gameover = False
+timer = 0;
+
+xpos = 500
+ypos = 500
